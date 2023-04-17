6bd600a2620d998aafe67691edada5f79761bf63
via23-ma <122565109+via23-ma@users.noreply.github.com>
2023-04-18 01:34:26 +0530

    add files via upload

---
+++ background.js
@@ -0,0 +1,18 @@
+﻿chrome.commands.onCommand.addListener((c) => {
+  switch(c){
+  case "command1":
+    chrome.tabs.create({url: "https://www.codechef.com/"}, ()=>{});
+    break;
+  case "command2":
+    chrome.tabs.create({url: "https://github.com/"}, ()=>{});
+    break;
+  case "command3":
+    chrome.tabs.create({url: "https://www.youtube.com/"}, ()=>{});
+    break;  
+  case "command4":
+    chrome.tabs.create({url: "https://www.linkedin.com/"}, ()=>{});
+    break;  
+  }
+  
+  
+});
---
+++ myscript.js
@@ -0,0 +1,9 @@
+﻿window.onload = () => {
+  chrome.commands.getAll((c) => {
+    let text = "";
+    for (let {name, shortcut, description} of c){
+      text += `Use  ${shortcut}  ${ description}\n`;
+    }
+    document.getElementById("id_text").value = text;
+  });
+}
